90920016977852835ee440409086b226ce98c680
bartuteber <bartuteber@gmail.com>
2026-06-16 15:16:17 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ToastBuilder from './containers/ToastBuilder.vue';
+</script>
+
+<template>
+<div>
+  <ToastBuilder />
+</div>
+</template>
+
+<style scoped></style>
---
+++ src/containers/ToastBuilder.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<template>
+<div style="height: 100%; width: 100%; align-items: center;">
+  <h1>Toast Notification Builder</h1>
+</div>
+</template>
